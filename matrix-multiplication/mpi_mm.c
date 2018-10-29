32db2f2f6c3f7b9dc50e19d2ed579f0a2691ef5d
J. Alejandro Cardona <alejandrocardona@outlook.com>
2018-10-29 17:53:32 -0500

    mpi_mm.c updated

--- matrix-multiplication/mpi_mm.c
+++ matrix-multiplication/mpi_mm.c
@@ -1,34 +1,35 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "matrix_helpers.h"
 
 #define MASTER 0
 #define FROM_MASTER 1
 #define FROM_WORKER 2
 
 // Performs the multiplication of a given matrix segment
-void MMSegment(float **m1, float **m2, float* result, int from, int to, int size)
-{
-    //printf("[%d] From: %d To: %d\n", getpid(), from, to);
-    for(int i = from; i < to; i++)
-    {
-        for(int j = 0; j < size; j++)
-        {
-            float cellTotal = 0;
-            for(int k = 0; k < size; k++)
-            {
-                cellTotal += m1[i][k] * m2[k][j];
-            }
-            result[i * size + j] = cellTotal;
-        }
-    }
-}
+// void MMSegment(double **m1, double **m2, double* result, int from, int to, int size)
+// {
+//     //printf("[%d] From: %d To: %d\n", getpid(), from, to);
+//     for(int i = from; i < to; i++)
+//     {
+//         for(int j = 0; j < size; j++)
+//         {
+//             double cellTotal = 0;
+//             for(int k = 0; k < size; k++)
+//             {
+//                 cellTotal += m1[i][k] * m2[k][j];
+//             }
+//             result[i * size + j] = cellTotal;
+//         }
+//     }
+// }
 
 int main(int argc, char ** argv) {
 
+    printf("Debug 1");
+
     int size = 1000;
-    int numtasks, taskid, numworkers;
+    int numtasks, taskid, numworkers, rows;
 
     double matrix1[size][size],
            matrix2[size][size],
@@ -45,20 +46,30 @@ int main(int argc, char ** argv) {
 
     if (numtasks < 2) {
         printf("Se necesitan almenos 2 'MPI tasks'");
-        MPI_Abort(MPI_COMM_WORLD, rc);
+        int errC;
+        MPI_Abort(MPI_COMM_WORLD, errC);
         exit(1);
     }
 
-    numworkers = numtaks - 1;
+    numworkers = numtasks - 1;
 
     if (taskid == MASTER)
     {
         printf("MPI ha iniciado con %d tareas.\n", numtasks);
 
-        FillMatrix(matrix1, size);
-        FillMatrix(matrix2, size);
+        for (int i=0; i < size; ++i) {
+            for (int j=0; j < size; ++j) {
+                matrix1[i][j] = ((double)rand() / (double)(RAND_MAX)) * 9.0;
+            }
+        }
+        for (int i=0; i < size; ++i) {
+            for (int j=0; j < size; ++j) {
+                matrix2[i][j] = ((double)rand() / (double)(RAND_MAX)) * 9.0;
+            }
+        }
 
-        // TODO: Mesure time
+        double start = MPI_Wtime();
+
         int dest = 1;
         for (dest = 1; dest <= numworkers; dest++) {
             int rowsToSend = (dest <= extra) ? averow + 1 : averow;
@@ -71,16 +82,15 @@ int main(int argc, char ** argv) {
         }
 
       int rowsRecv, source;
-      for (i = 1; i <= numworkers; i++)
+      for (int i = 1; i <= numworkers; i++)
       {
           source = i;
-          MPI_Recv(&offset, 1, MPI_INT, source, FORM_WORKER, MPI_COMM_WORLD, &status);
-          MPI_Recv(&rowsRecv, 1, MPI_INT, source, FORM_WORKER, MPI_COMM_WORLD, &status);
-          MPI_Recv(&matrixR[offset][0], rowsRecv*size, MPI_DOUBLE, source, FORM_WORKER, MPI_COMM_WORLD, &status);
+          MPI_Recv(&offset, 1, MPI_INT, source, FROM_WORKER, MPI_COMM_WORLD, &status);
+          MPI_Recv(&rowsRecv, 1, MPI_INT, source, FROM_WORKER, MPI_COMM_WORLD, &status);
+          MPI_Recv(&matrixR[offset][0], rowsRecv*size, MPI_DOUBLE, source, FROM_WORKER, MPI_COMM_WORLD, &status);
       }
 
-      // TODO: Measure time
-      // double finish = MPI_Wtime();
+      double finish = MPI_Wtime();
       printf("Done in %f seconds.\n", finish - start);
     }
 
@@ -92,11 +102,11 @@ int main(int argc, char ** argv) {
         MPI_Recv(&matrix1, rows*size, MPI_DOUBLE, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status);
         MPI_Recv(&matrix2, size*size, MPI_DOUBLE, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status);
 
-        for (k=0; k<NCB; k++) {
-            for (i=0; i<rows; i++) {
-                c[i][k] = 0.0;
-                for (j=0; j<NCA; j++) {
-                    c[i][k] = c[i][k] + a[i][j] * b[j][k];
+        for (int k=0; k<size; k++) {
+            for (int i=0; i<rows; i++) {
+                matrixR[i][k] = 0.0;
+                for (int j=0; j<size; j++) {
+                    matrixR[i][k] = matrixR[i][k] + matrix1[i][j] * matrix2[j][k];
                 }
             }
         }
